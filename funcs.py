fa2947a0ed6adc8801e62d2350770910fd902bb3
George Pentchev <georgiepentch@gmail.com>
2023-04-17 16:25:44 -0400

    initial commit

---
+++ funcs.py
@@ -0,0 +1,86 @@
+rows4 = [[int(j) for j in format(i, "04b")] for i in range(2 ** 4)]
+rows5 = [[int(j) for j in format(i, "05b")] for i in range(2 ** 5)]
+rows6 = [[int(j) for j in format(i, "06b")] for i in range(2 ** 6)]
+
+
+def apply(t):
+    res = [0]*len(t)
+    if t[0] == 1:
+        res[0] = (res[0] + 1) % 2
+        res[1] = (res[1] + 1) % 2
+    for i in range(1, len(t) - 1):
+        if t[i] == 1:
+            res[i - 1] = (res[i - 1] + 1) % 2
+            res[i] = (res[i] + 1) % 2
+            res[i + 1] = (res[i + 1] + 1) % 2
+    if t[len(t) - 1] == 1:
+        res[len(t) - 2] = (res[len(t) - 2] + 1) % 2
+        res[len(t) - 1] = (res[len(t) - 1] + 1) % 2
+    return res
+
+
+trows4 = [(i, apply(i)) for i in rows4]
+trows5 = [(i, apply(i)) for i in rows5]
+trows6 = [(i, apply(i)) for i in rows6]
+
+
+def findt(row):
+    dic = []
+    if len(row) == 4:
+        dic = trows4
+    elif len(row) == 5:
+        dic = trows5
+    elif len(row) == 6:
+        dic = trows6
+
+    for i in dic:
+        if i[1] == row:
+            return i[0]
+
+
+def transpose(board):
+    return [list(i) for i in zip(*board)]
+
+
+def solve(board):
+    sol = [findt(row) for row in board]
+    sol = transpose(sol)  # transpose sol
+    sol = [findt(row) for row in sol]
+    sol = transpose(sol) # transpose sol again
+    if len(board) == 5:
+        sol = optimize5(sol)
+    return sol
+
+
+def null5(row):
+    row[0] = int(not row[0])
+    row[1] = int(not row[1])
+    row[3] = int(not row[3])
+    row[4] = int(not row[4])
+
+
+def count1s(row):
+    return len([i for i in row if i == 1])
+
+
+def optimize5(sol):
+    changed = False
+
+    for row in sol:
+        if count1s(row[:2]) + count1s(row[3:]) >= 2:
+            null5(row)
+            changed = True
+
+    if not changed:
+        return sol
+    else:
+        return optimize5(transpose(sol))
+
+
+print(solve([
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]))
---
+++ main.py
@@ -0,0 +1,109 @@
+import pygame
+from funcs import *
+import numpy as np
+from math import sqrt
+
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
+
+
+class Tile:
+    def __init__(self, x, y, state):
+        self.x = x
+        self.y = y
+        self.color = Tile.default
+        self.clicked = False
+        self.rect = None
+        self.state = state
+
+    default = (194, 147, 60)
+    pressed = (99, 186, 86)
+    highlight = (230, 123, 62)
+
+    def draw(self):
+
+        self.rect = pygame.draw.rect(screen, self.color, (100*(self.x+1), 100*(self.y+1), 100, 100))  # fill
+        pygame.draw.rect(screen, (0, 0, 0), (100*(self.x+1)-2, 100*(self.y+1)-2, 104, 104), 4)  # border
+
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and not self.clicked:
+                self.clicked = True
+                if self.color == Tile.default:
+                    self.color = Tile.pressed
+                    self.state = 1
+                else:
+                    self.color = Tile.default
+                    self.state = 0
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+    @classmethod
+    def grid(cls, n):
+        res = []
+        for i in range(n):
+            for j in range(n):
+                res += [cls(j, i, 0)]
+        return res
+
+    @staticmethod
+    def drawgrid(grid):
+        for i in grid:
+            i.draw()
+
+    @staticmethod
+    def getstates(grid):
+        return [i.state for i in grid]
+
+    @staticmethod
+    def reset(grid):
+        for i in grid:
+            i.state = 0
+            i.color = Tile.default
+
+
+grid6 = Tile.grid(6)
+grid5 = Tile.grid(5)
+grid4 = Tile.grid(4)
+
+currentgrid = grid6
+
+
+def solvepos(gridstates):
+    a = np.array(gridstates)
+    b = np.reshape(a, (-1, int(sqrt(len(gridstates))))).tolist()
+    c = solve(b)
+    flat_c = [item for sublist in c for item in sublist]
+    occurs = [i for i, x in enumerate(flat_c) if x == 1]
+    return occurs
+
+
+run = True
+# game loop
+while run:
+
+    # fill
+    screen.fill((241, 230, 136))
+
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_4:
+                currentgrid = grid4
+            if event.key == pygame.K_5:
+                currentgrid = grid5
+            if event.key == pygame.K_6:
+                currentgrid = grid6
+            if event.key == pygame.K_r:
+                Tile.reset(currentgrid)
+            if event.key == pygame.K_s:
+                for i in solvepos(Tile.getstates(currentgrid)):
+                    currentgrid[i].color = Tile.highlight
+
+    Tile.drawgrid(currentgrid)
+
+    pygame.display.update()
